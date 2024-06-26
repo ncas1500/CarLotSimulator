dd435f009470753a602a25a85da4e21aa4c396c2
narciso castro <narcisocastro0802@gmail.com>
2024-06-25 23:56:22 -0700

    Added classes and methods

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public Car(int year, string make, string model, string engineNoise, string honkNoise,bool isDriveable)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDriveable = isDriveable;
+        }
+
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public void MakeEngineNoise(string noise)
+        {
+            Console.WriteLine($"The noise this car engine makes is {noise}.");
+        }
+
+        public void MakeHonkNoise(string noise)
+        {
+            Console.WriteLine($"The horn on this car is {noise}.");
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
 
 namespace CarLotSimulator
 {
@@ -6,6 +8,52 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
+
+            var lot = new CarLot();
+            
+
+            var carOne = new Car();
+            carOne.Year = 2024;
+            carOne.Make = "Genesis";
+            carOne.Model = "G90";
+            carOne.EngineNoise = "Quiet";
+            carOne.HonkNoise = "Loud";
+            carOne.IsDriveable = true;
+
+            lot.Cars.Add(carOne);
+
+            var carTwo = new Car()
+            {
+                Year = 2024,
+                Make = "Cadillac",
+                Model = "Escalade",
+                EngineNoise = "Quiet",
+                HonkNoise = "Loud",
+                IsDriveable = true,
+            };
+
+            lot.Cars.Add(carTwo);
+
+            var carThree = new Car(2023, "Lexus", "LS", "Quiet", "Loud", true);
+
+            lot.Cars.Add(carThree);
+
+            carOne.MakeEngineNoise(carOne.EngineNoise);
+            carOne.MakeHonkNoise(carOne.HonkNoise);
+            carTwo.MakeEngineNoise(carTwo.EngineNoise);
+            carTwo.MakeHonkNoise(carThree.HonkNoise);
+            carThree.MakeEngineNoise(carThree.EngineNoise);
+            carThree.MakeHonkNoise(carThree.HonkNoise);
+
+
+            foreach (var car in lot.Cars)
+            {
+                Console.WriteLine($"Year: {car.Year} Make: {car.Make} Model: {car.Model} ");
+                
+            }
+
+
+
             //TODO
 
             //Create a seperate class file called Car
@@ -17,6 +65,7 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+
 
             //*************BONUS*************//
 
